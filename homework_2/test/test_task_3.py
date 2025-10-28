b5076186c3ef750f5bcacb7187cec527184b5e7b
Nikita Zekynov <nzekynov@mail.ru>
2025-10-28 19:39:15 +0300

    Tasks 3 and 4 for homework 2

---
+++ homework_2/test/test_task_3.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    opts = Options()
+
+    opts.add_argument("--headless=new")
+    opts.add_argument("--window-size=1280,900")
+    driver = webdriver.Chrome(options=opts)
+    yield driver
+
+    driver.quit()
+
+
+@pytest.mark.selenium
+def test_selenium_web_open_google(driver):
+    url = "https://www.google.com/"
+    driver.get(url)
+    assert driver.title == "Google"
+    assert driver.current_url == url
+
+
+@pytest.mark.selenium
+def test_selenium_web_open_github(driver):
+    url = "https://github.com/"
+    driver.get(url)
+    assert driver.current_url == url
+    assert (
+        driver.title
+        == "GitHub · Build and ship software on a single, collaborative platform · GitHub"
+    )
